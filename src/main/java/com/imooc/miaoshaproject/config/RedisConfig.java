a3ae796abef9b1039bb6da33c7b0f80a3758ac17
shenzhouyh <286817442@qq.com>
2020-07-11 00:51:43 +0800

    新增redis依赖，用于实现分布式session

---
+++ src/main/java/com/imooc/miaoshaproject/config/RedisConfig.java
@@ -0,0 +1,14 @@
+package com.imooc.miaoshaproject.config;
+
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.stereotype.Component;
+
+/**
+ * @description : 配置redis的个性化参数
+ * @since : 10.7.0
+ */
+@Component
+//会话时长默认是1800（30分钟），可更改为3600（一个小时）
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600)
+public class RedisConfig {
+}
