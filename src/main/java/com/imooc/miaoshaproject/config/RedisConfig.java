895cb26fb7b1018d8e0be9d8d196e799a6c7df6f
shenzhouyh <286817442@qq.com>
2020-07-14 00:30:19 +0800

    定制化redis配置，修改序列化参数

--- src/main/java/com/imooc/miaoshaproject/config/RedisConfig.java
+++ src/main/java/com/imooc/miaoshaproject/config/RedisConfig.java
@@ -1,7 +1,15 @@
 package com.imooc.miaoshaproject.config;
 
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @description : 配置redis的个性化参数
@@ -11,4 +19,17 @@ import org.springframework.stereotype.Component;
 /* 会话时长默认是1800（30分钟），可更改为3600（一个小时） */
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600)
 public class RedisConfig {
+    //定制化获取redisTemplate，可参考RedisAutoConfiguration.java
+    @Bean
+    public RedisTemplate getRedisTemplate(RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        //修改redis中的key序列化方式
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        //解决value的序列化方式（一般使用json）
+        Jackson2JsonRedisSerializer jsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        redisTemplate.setValueSerializer(jsonRedisSerializer);
+        return redisTemplate;
+    }
 }
